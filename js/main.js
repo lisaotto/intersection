44171323cde5efb8dde47b2686d96a57212a8749
lisaotto <otto.elizabeth@gmail.com>
2015-10-11 21:59:51 -0400

    setting up

---
+++ js/main.js
@@ -0,0 +1,26 @@
+
+
+var config1 = {
+  "id": '653220207209529344',
+  "domId": 'pedestrian',
+  "maxTweets": 0,
+  "enableLinks": true
+};
+twitterFetcher.fetch(config1);
+
+
+var config2 = {
+  "id": '653222188724584449',
+  "domId": 'driver',
+  "maxTweets": 20,
+  "enableLinks": true
+};
+twitterFetcher.fetch(config2);
+
+var config3= {
+  "id": '653373323435491329',
+  "domId": 'ugh',
+  "maxTweets": 0,
+  "enableLinks": true
+};
+twitterFetcher.fetch(config3);
